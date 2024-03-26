677d6e1601e62937c90ef963285d1a69d8d0b375
Yikai Fang <yfang32@student.ubc.ca>
2024-03-26 00:18:06 -0700

    Finished addCustomerMenuGUI and finished setting up frame

--- src/main/ui/gui/AddCustomerMenuGUI.java
+++ src/main/ui/gui/AddCustomerMenuGUI.java
@@ -1,6 +1,66 @@
 package ui.gui;
 
+import model.Car;
+import model.Customer;
+
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddCustomerMenuGUI extends JFrame {
+
+    private JTextField nameField;
+    private JTextField emailField;
+    private JTextField phoneNumberField;
+    private JTextField carVinField;
+    private JTextField carModelField;
+    private JTextField carMakeField;
+    private DefaultListModel<String> customers;
+    private List<Customer> shopCustomers;
+
+    // EFFECTS: constructs the main menu and runs the method mainMenu() and also instantiating a list of customers
+    public AddCustomerMenuGUI(DefaultListModel<String> customers, List<Customer> shopCustomers) {
+        initializeTextFields();
+        this.customers = customers;
+        this.shopCustomers = shopCustomers;
+
+        Object[] fields = {
+                "name", nameField,
+                "email", emailField,
+                "phone number", phoneNumberField,
+                "car vin", carVinField,
+                "car model", carModelField,
+                "car make", carMakeField
+        };
+
+        JOptionPane.showConfirmDialog(null, fields, "Please enter:", JOptionPane.OK_CANCEL_OPTION);
+        String name = nameField.getText();
+        String email = emailField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        String vin = carVinField.getText();
+        String model = carModelField.getText();
+        String make = carMakeField.getText();
+        Customer customer = new Customer(name, phoneNumber, email);
+        Car car = new Car(vin, model, make);
+        customer.addCar(car);
+        this.shopCustomers.add(customer);
+        this.customers.addElement(informationString(customer, car));
+    }
+
+    private String informationString(Customer customer, Car car) {
+        ArrayList<String> cars = new ArrayList<>();
+        for (Car c : customer.getCars()) {
+            cars.add(car.getYear() + " " + c.getMake() + " " + c.getModel());
+        }
+        return customer.getName() + " | " + customer.getPhoneNumber() + " | " + customer.getEmail() + " | " + cars;
+    }
+
+    private void initializeTextFields() {
+        nameField = new JTextField();
+        emailField = new JTextField();
+        phoneNumberField = new JTextField();
+        carVinField = new JTextField();
+        carMakeField = new JTextField();
+        carModelField = new JTextField();
+    }
 }
--- src/main/ui/gui/MainMenuGUI.java
+++ src/main/ui/gui/MainMenuGUI.java
@@ -5,27 +5,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
 import javax.swing.*;
 
-import model.Car;
 import model.CarShop;
-import model.Customer;
 import persistance.JsonReader;
 import persistance.JsonWriter;
-import ui.tools.AddCustomerMenu;
 import ui.tools.CustomerEditorMenu;
 
 /*
 Main menu of the application where user is given different choices to choose from each leading to different menus.
  */
 
-public class MainMenuGUI extends JFrame {
+public class MainMenuGUI extends JFrame implements ActionListener {
 
     private static final String JSON_STORE = "./data/carShop.json";
-    private Scanner input;
     private CarShop carShop;
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
@@ -34,6 +27,7 @@ public class MainMenuGUI extends JFrame {
     private JButton removeCustomerButton;
     private JButton saveButton;
     private JButton loadButton;
+    private JButton surpriseButton;
     private JList<String> customerList;
     private DefaultListModel<String> customers;
     public static final int WIDTH = 1000;
@@ -43,16 +37,18 @@ public class MainMenuGUI extends JFrame {
     public MainMenuGUI() throws FileNotFoundException {
         super("South Auto Center");
         initializeFields();
+        initializeSearchBar();
         initializeGraphics();
     }
 
+
     private void initializeGraphics() {
-        setLayout(new BorderLayout());
+        setLayout(null);
         setMinimumSize(new Dimension(WIDTH, HEIGHT));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
-        runMainMenu();
+        displayMenu();
     }
 
     private void initializeFields() {
@@ -64,85 +60,18 @@ public class MainMenuGUI extends JFrame {
         removeCustomerButton = new JButton("remove customer");
         saveButton = new JButton("save");
         loadButton = new JButton("load");
+        surpriseButton = new JButton("press for suprise");
         this.add(addCustomerButton);
         this.add(editCustomerButton);
         this.add(removeCustomerButton);
         this.add(saveButton);
         this.add(loadButton);
+        this.add(surpriseButton);
         customers = new DefaultListModel<>();
         customerList = new JList<>(customers);
-        customerList.setBounds(300, 0, 700, 700);
+        customerList.setBounds(300, 25, 700, 675);
+        customerList.setFont(new Font("Fira Sans", Font.PLAIN, 18));
         this.add(customerList);
-
-    }
-
-    // inspired by TellerApp https://github.students.cs.ubc.ca/CPSC210/TellerApp.git
-    // EFFECTS: actually starts the main menu and shows all the options that user can pick from.
-    public void runMainMenu() {
-        input = new Scanner(System.in);
-        input.useDelimiter("\n");
-        boolean isStillRunning = true;
-        String input;
-
-        while (isStillRunning) {
-            printAllCustomers();
-            displayMenu();
-            input = this.input.next().toLowerCase();
-
-            if (input.equals("q")) {
-                isStillRunning = false;
-            } else {
-                processInput(input);
-            }
-        }
-
-        System.out.println("Closing application");
-
-    }
-
-    // EFFECTS: Processes the input of the user if it doesn't match any of the given options nothing will happen.
-    private void processInput(String input) {
-        switch (input) {
-            case "add customer":
-                addCustomer();
-                break;
-            case "r":
-                removeCustomer();
-                break;
-            case "e":
-                editCustomer();
-                break;
-            case "s":
-                searchForCustomer();
-                break;
-            case "save":
-                saveCustomers();
-                break;
-            case "load":
-                loadCustomers();
-                break;
-            default:
-                System.out.println("Invalid option, must choose one of the options listed above.");
-                break;
-        }
-    }
-
-    //REQUIRES: !customers.isEmpty();
-    //EFFECTS : Finds the customer in the list of all the customers with the name and prints out
-    //          all the information about that customer.
-    private void searchForCustomer() {
-        Customer nullCustomer = new Customer(null, null, null);
-        System.out.println("Please enter customer name");
-        String name = input.next();
-        Customer chosenCustomer = nullCustomer.findCustomer(carShop.getCustomers(), name);
-        ArrayList<String> customerCars = new ArrayList<>();
-        for (Car car : chosenCustomer.getCars()) {
-            customerCars.add(car.getYear() + " " + car.getMake() + " " + car.getModel());
-        }
-        System.out.println("Result: " + chosenCustomer.getName() + " | " + chosenCustomer.getEmail()
-                + " | " + chosenCustomer.getPhoneNumber() + " | " + customerCars);
-        displayMenu();
-        processInput(input.next());
     }
 
     //EFFECTS: opens the customer editor menu
@@ -155,42 +84,21 @@ public class MainMenuGUI extends JFrame {
     //          however would like to change this to a button when implementing a GUI cannot remove all
     //          the customers.
     private void removeCustomer() {
-        String removedCustomer = "";
-        System.out.println("Please enter name of customer you would like to remove: ");
-        String customerName = input.next();
-        ArrayList<Customer> customers = carShop.getCustomers();
 
-        for (int i = 0; i < customers.size(); i++) {
-            if (customers.get(i).getName().equals(customerName)) {
-                removedCustomer = customers.get(i).getName();
-                customers.remove(customers.get(i));
-            }
-        }
+    }
 
-        if (Objects.equals(removedCustomer, "")) {
-            System.out.println("No customer with name: " + customerName);
-        } else {
-            System.out.println("Removed customer: " + removedCustomer);
-        }
+    private void initializeSearchBar() {
+        JTextField searchBar = new JTextField();
+        this.add(searchBar);
+        searchBar.setBounds(300, 0, 700, 25);
+        searchBar.setFont(new Font("Fira Sans", Font.PLAIN, 16));
+        searchBar.setText("search for customer");
+        searchBar.setBackground(Color.lightGray);
     }
 
     // EFFECTS: Opens the add customer menu and prints out the list of all customers names.
     private void addCustomer() {
-        new AddCustomerMenu(carShop.getCustomers());
-    }
-
-    // REQUIRES: customers is not an empty list
-    // EFFECTS : prints out a list of all the customers with their personal info and a list of their cars
-    public void printAllCustomers() {
-        System.out.println("All customers: ");
-
-        for (Customer c : carShop.getCustomers()) {
-            ArrayList<String> cars = new ArrayList<>();
-            for (Car car : c.getCars()) {
-                cars.add(car.getYear() + " " + car.getMake() + " " + car.getModel());
-            }
-            System.out.println(c.getName() + " | " + c.getEmail() + " | " + c.getPhoneNumber() + " | " + cars);
-        }
+        new AddCustomerMenuGUI(customers, carShop.getCustomers());
     }
 
     // EFFECTS: saves the workroom to file
@@ -216,55 +124,45 @@ public class MainMenuGUI extends JFrame {
         }
     }
 
+    private void surprise() {
+    }
+
     // inspired by TellerApp https://github.students.cs.ubc.ca/CPSC210/TellerApp.git
     // EFFECTS: displays all the choices with the format input key -> what it does.
     private void displayMenu() {
-        addCustomerButton.setSize(300,50);
+        addCustomerButton.setSize(300, 50);
         addCustomerButton.setBounds(0, 0, 300, 50);
-        addCustomerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource().equals(addCustomerButton)) {
-                    addCustomer();
-                }
-            }
-        });
+        addCustomerButton.addActionListener(this);
         editCustomerButton.setBounds(0, 50, 300, 50);
         editCustomerButton.setSize(300, 50);
-        editCustomerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (customerList.getSelectedIndex() != -1) {
-                    editCustomer();
-                }
-            }
-        });
-        removeCustomerButton.setSize(300,50);
+        editCustomerButton.addActionListener(this);
+        removeCustomerButton.setSize(300, 50);
         removeCustomerButton.setBounds(0, 100, 300, 50);
-        removeCustomerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (customerList.getSelectedIndex() != -1) {
-                    removeCustomer();
-                }
-            }
-        });
-        saveButton.setSize(300,50);
+        removeCustomerButton.addActionListener(this);
+        saveButton.setSize(300, 50);
         saveButton.setBounds(0, 150, 300, 50);
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                saveCustomers();
-            }
-        });
-        loadButton.setSize(300,50);
+        saveButton.addActionListener(this);
+        loadButton.setSize(300, 50);
         loadButton.setBounds(0, 200, 300, 50);
-        loadButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                loadCustomers();
-            }
-        });
+        loadButton.addActionListener(this);
+        surpriseButton.setBounds(0, 250, 300, 450);
+        surpriseButton.addActionListener(this);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(addCustomerButton)) {
+            addCustomer();
+        } else if (e.getSource().equals(editCustomerButton)) {
+            editCustomer();
+        } else if (e.getSource().equals(removeCustomerButton)) {
+            removeCustomer();
+        } else if (e.getSource().equals(saveButton)) {
+            saveCustomers();
+        } else if (e.getSource().equals(loadButton)) {
+            loadCustomers();
+        } else if (e.getSource().equals(surpriseButton)) {
+            surprise();
+        }
     }
 }
